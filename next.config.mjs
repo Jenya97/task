52db8bfffed43c42c29f46caf87fb3bcde440676
Jenya97 <grigoryan_jenya@bk.ru>
2024-09-10 22:25:39 +0400

    hh

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Convert `import.meta.url` to the current directory path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Ensure this line is present to export static files
+  output: 'export',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -18,13 +17,13 @@ const nextConfig = {
     return config;
   },
   images: {
-    unoptimized: true, // Helps with image handling in static exports
+    unoptimized: true,
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  basePath: '/task', // Add '/' before the repository name
-  assetPrefix: '/task', // Add '/' before the repository name
+  basePath: '/task',
+  assetPrefix: '/task',
 };
 
 export default nextConfig;
