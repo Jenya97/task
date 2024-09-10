ee37946fb5096fff7601923b6758e1a5f63f2698
Jenya97 <grigoryan_jenya@bk.ru>
2024-09-10 22:21:08 +0400

    hh

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-// next.config.js
+// next.config.mjs
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Convert `import.meta.url` to the current directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
